f0d84dce2ded078618d37e0e09473e6d0403fccb
PietroManganelliConforti <manganelliconforti.1754825@studenti.uniroma1.it>
2023-07-11 12:31:15 +0200

    Fixed plots, added the smoothed version with splines

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import torch
 import matplotlib.pyplot as plt
 import os
 import numpy as np
+from scipy.interpolate import make_interp_spline
 
 def hardware_check():
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
@@ -74,29 +75,38 @@ def evaluate_model(model, loader):
 
 
 
-def save_plot_loss_or_rel_err(info_dict, path, test_name):
+def save_plot_loss_or_rel_err(info_dict, n_of_elements, path, test_name, smooth=False):
     """
         ret = {
             "losses" : {
-                "loss_train" : [],
-                "loss_eval" : [],
-                "loss_test" : []
+                "loss_train" : [5,4,3,2,1],
+                "loss_eval" : [6,5,4,3,2],
+                "loss_test" : [2.5]
             },
             "acc" : {
-                "acc_train":[],
-                "acc_eval":[],
-                "acc_test":[]   
+                "acc_train":[10,9,8,7,6],
+                "acc_eval":[11,10,9,8,7],
+                "acc_test":[7.8]   
             }
         }
     """
 
 
     for k in list(info_dict.keys()):
-        #if k == "acc_test" or k == "loss_test":
-        #    plt.plot(np.arange(len(info_dict[acc_or_loss]["loss_train"])) , info_dict[acc_or_loss][k], '-x', label=k)   #per posizionare il test in fondo 
-        #else:
         
-        plt.plot(info_dict[k], '-x', label=k)
+        if k == "loss_test" or k == "rel_err_test":
+            plt.plot(n_of_elements - 1 , info_dict[k], '-x', label=k)   #per posizionare il test in fondo 
+        
+        else:
+            if smooth:
+                idx= range(n_of_elements) 
+                spl = make_interp_spline(idx, info_dict[k], k=3)
+                xnew = np.linspace(min(idx), max(idx), 300)
+                smoothing = spl(xnew)
+
+                plt.plot(xnew, smoothing, '-', label=k)
+            else:    
+                plt.plot(info_dict[k], '-', label=k)
 
                 
     plt.legend()
@@ -105,16 +115,18 @@ def save_plot_loss_or_rel_err(info_dict, path, test_name):
     plt.title(  str(list(info_dict.keys())[0]).split("_")[0] + " vs. No. of epochs" )
 
     if not os.path.exists(path):
+        print("PATH: ",path)
         os.makedirs(path)
 
-    plt.savefig(path+test_name+".png")
+    if smooth: plt.savefig(path+test_name+"_smoothed.png")
+    else: plt.savefig(path+test_name+".png")
     plt.clf()
     plt.close()
 
 
     
 
-def save_plots_and_report(ret_dict, save_path, test_name):
+def save_plots_and_report(ret_dict, save_path, test_name, smooth= False):
 
     ret_str ="loss_train: " + str(ret_dict["losses"]["loss_train"][-1:])
     ret_str +="\n\nloss_eval: " + str(ret_dict["losses"]["loss_eval"][-1:]) 
@@ -126,5 +138,27 @@ def save_plots_and_report(ret_dict, save_path, test_name):
     with open(save_path+test_name +'.txt', 'w+') as f:
         f.write(test_name + "\n\n"+ ret_str + '\n\nret dict:\n' + str(ret_dict))
 
-    save_plot_loss_or_rel_err( ret_dict["losses"], path = save_path + "/loss/" , test_name = "loss" )
-    save_plot_loss_or_rel_err( ret_dict["rel_err"], path = save_path + "/rel_err/" , test_name = "rel_err" )
+    n_of_elements = len(ret_dict["losses"]["loss_train"]) 
+
+    save_plot_loss_or_rel_err( ret_dict["losses"], n_of_elements ,path = save_path + "loss/" , test_name = "loss" , smooth= smooth)
+    save_plot_loss_or_rel_err( ret_dict["rel_err"], n_of_elements ,path = save_path + "rel_err/" , test_name = "rel_err" , smooth= smooth)
+    
+    if smooth:
+        save_plot_loss_or_rel_err( ret_dict["losses"], n_of_elements ,path = save_path + "loss/" , test_name = "loss" , smooth= False)
+        save_plot_loss_or_rel_err( ret_dict["rel_err"], n_of_elements ,path = save_path + "rel_err/" , test_name = "rel_err" , smooth= False)
+
+
+test_ret = {
+            "losses" : {
+                "loss_train" : [5,40,3,20,1],
+                "loss_eval" : [6,50,4,30,2],
+                "loss_test" : [2.5]
+            },
+            "rel_err" : {
+                "rel_err_train":[10,90,8,70,6],
+                "rel_err_eval":[11,100,9,80,7],
+                "rel_err_test":[7.8]   
+            }
+        }
+
+save_plots_and_report(test_ret,"","da_canc", True)
