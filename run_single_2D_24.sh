95d0eaa232f7dac230ca287e66133e4654f8459b
PietroManganelliConforti <manganelliconforti.1754825@studenti.uniroma1.it>
2024-01-17 18:48:38 +0100

    fixed run_single script

--- run_single_2D_24.sh
+++ run_single_2D_24.sh
@@ -8,13 +8,13 @@ python3 main.py 2D --dataset_path datasets/2D --output_var 'CO(GT)' --transform 
     --mode 'forecasting_lstm' --gpu 0 --bs 8\
     --variables-to-use 'CO(GT)' 'PT08.S1(CO)' 'C6H6(GT)' 'PT08.S2(NMHC)' \
     'NOx(GT)' 'PT08.S3(NOx)' 'NO2(GT)' 'PT08.S4(NO2)' 'PT08.S5(O3)' 'T' 'RH' 'AH' \
-    --cross_val 5
+    --cross_val 5 --augmentation
 
 python3 main.py 2D --dataset_path datasets/2D --output_var 'CO(GT)' --transform 'ricker'\
     --mode 'forecasting_lstm' --gpu 0 --bs 8\
     --variables-to-use 'CO(GT)' 'PT08.S1(CO)' 'C6H6(GT)' 'PT08.S2(NMHC)' \
     'NOx(GT)' 'PT08.S3(NOx)' 'NO2(GT)' 'PT08.S4(NO2)' 'PT08.S5(O3)' 'T' 'RH' 'AH' \
-    --cross_val 5
+    --cross_val 5 --augmentation
 
 
 #### CONFIGURAZIONI VIT #### 
@@ -23,21 +23,21 @@ python3 main.py 2D --dataset_path datasets/2D --output_var 'CO(GT)' --transform 
     --mode 'ViT_im' --gpu 0 --bs 8\
     --variables-to-use 'CO(GT)' 'PT08.S1(CO)' 'C6H6(GT)' 'PT08.S2(NMHC)' \
     'NOx(GT)' 'PT08.S3(NOx)' 'NO2(GT)' 'PT08.S4(NO2)' 'PT08.S5(O3)' 'T' 'RH' 'AH' \
-    --cross_val 5
+    --cross_val 5 --augmentation
 
 
 python3 main.py 2D --dataset_path datasets/2D --output_var 'CO(GT)' --transform 'ricker'\
     --mode '2D_ViT_parallel_SR' --gpu 0 --bs 8\
     --variables-to-use 'CO(GT)' 'PT08.S1(CO)' 'C6H6(GT)' 'PT08.S2(NMHC)' \
     'NOx(GT)' 'PT08.S3(NOx)' 'NO2(GT)' 'PT08.S4(NO2)' 'PT08.S5(O3)' 'T' 'RH' 'AH' \
-    --cross_val 5
+    --cross_val 5 --augmentation
 
 
 python3 main.py 2D --dataset_path datasets/2D --output_var 'CO(GT)' --transform 'ricker'\
     --mode '2D_ViT_SR_feat_in' --gpu 0 --bs 8\
     --variables-to-use 'CO(GT)' 'PT08.S1(CO)' 'C6H6(GT)' 'PT08.S2(NMHC)' \
     'NOx(GT)' 'PT08.S3(NOx)' 'NO2(GT)' 'PT08.S4(NO2)' 'PT08.S5(O3)' 'T' 'RH' 'AH' \
-    --cross_val 5
+    --cross_val 5 --augmentation
 
 #### CONFIGURAZIONI VIT FREEZE ####
 
@@ -45,20 +45,20 @@ python3 main.py 2D --dataset_path datasets/2D --output_var 'CO(GT)' --transform 
     --mode 'ViT_im' --gpu 0 --bs 8\
     --variables-to-use 'CO(GT)' 'PT08.S1(CO)' 'C6H6(GT)' 'PT08.S2(NMHC)' \
     'NOx(GT)' 'PT08.S3(NOx)' 'NO2(GT)' 'PT08.S4(NO2)' 'PT08.S5(O3)' 'T' 'RH' 'AH' \
-    --freeze --cross_val 5
+    --freeze --cross_val 5 --augmentation
 
 python3 main.py 2D --dataset_path datasets/2D --output_var 'CO(GT)' --transform 'ricker'\
     --mode '2D_ViT_parallel_SR' --gpu 0 --bs 8\
     --variables-to-use 'CO(GT)' 'PT08.S1(CO)' 'C6H6(GT)' 'PT08.S2(NMHC)' \
     'NOx(GT)' 'PT08.S3(NOx)' 'NO2(GT)' 'PT08.S4(NO2)' 'PT08.S5(O3)' 'T' 'RH' 'AH' \
-    --freeze --cross_val 5
+    --freeze --cross_val 5 --augmentation
 
 
 python3 main.py 2D --dataset_path datasets/2D --output_var 'CO(GT)' --transform 'ricker'\
     --mode '2D_ViT_SR_feat_in' --gpu 0 --bs 8\
     --variables-to-use 'CO(GT)' 'PT08.S1(CO)' 'C6H6(GT)' 'PT08.S2(NMHC)' \
     'NOx(GT)' 'PT08.S3(NOx)' 'NO2(GT)' 'PT08.S4(NO2)' 'PT08.S5(O3)' 'T' 'RH' 'AH' \
-    --freeze --cross_val 5
+    --freeze --cross_val 5 --augmentation
 
 #### CONFIGURAZIONI VIT PRETRAINED ####
 
@@ -66,20 +66,20 @@ python3 main.py 2D --dataset_path datasets/2D --output_var 'CO(GT)' --transform 
     --mode 'ViT_im' --gpu 0 --bs 8\
     --variables-to-use 'CO(GT)' 'PT08.S1(CO)' 'C6H6(GT)' 'PT08.S2(NMHC)' \
     'NOx(GT)' 'PT08.S3(NOx)' 'NO2(GT)' 'PT08.S4(NO2)' 'PT08.S5(O3)' 'T' 'RH' 'AH' \
-    --pretrained --cross_val 5
+    --pretrained --cross_val 5 --augmentation
 
 python3 main.py 2D --dataset_path datasets/2D --output_var 'CO(GT)' --transform 'ricker'\
     --mode '2D_ViT_parallel_SR' --gpu 0 --bs 8\
     --variables-to-use 'CO(GT)' 'PT08.S1(CO)' 'C6H6(GT)' 'PT08.S2(NMHC)' \
     'NOx(GT)' 'PT08.S3(NOx)' 'NO2(GT)' 'PT08.S4(NO2)' 'PT08.S5(O3)' 'T' 'RH' 'AH' \
-    --pretrained --cross_val 5
+    --pretrained --cross_val 5 --augmentation
 
 
 python3 main.py 2D --dataset_path datasets/2D --output_var 'CO(GT)' --transform 'ricker'\
     --mode '2D_ViT_SR_feat_in' --gpu 0 --bs 8\
     --variables-to-use 'CO(GT)' 'PT08.S1(CO)' 'C6H6(GT)' 'PT08.S2(NMHC)' \
     'NOx(GT)' 'PT08.S3(NOx)' 'NO2(GT)' 'PT08.S4(NO2)' 'PT08.S5(O3)' 'T' 'RH' 'AH' \
-    --pretrained --cross_val 5
+    --pretrained --cross_val 5 --augmentation
 
 #### CONFIGURAZIONI VIT PRETRAINED ####
 
@@ -87,20 +87,20 @@ python3 main.py 2D --dataset_path datasets/2D --output_var 'CO(GT)' --transform 
     --mode 'ViT_im' --gpu 0 --bs 8\
     --variables-to-use 'CO(GT)' 'PT08.S1(CO)' 'C6H6(GT)' 'PT08.S2(NMHC)' \
     'NOx(GT)' 'PT08.S3(NOx)' 'NO2(GT)' 'PT08.S4(NO2)' 'PT08.S5(O3)' 'T' 'RH' 'AH' \
-    --pretrained --freeze --cross_val 5
+    --pretrained --freeze --cross_val 5 --augmentation
 
 python3 main.py 2D --dataset_path datasets/2D --output_var 'CO(GT)' --transform 'ricker'\
     --mode '2D_ViT_parallel_SR' --gpu 0 --bs 8\
     --variables-to-use 'CO(GT)' 'PT08.S1(CO)' 'C6H6(GT)' 'PT08.S2(NMHC)' \
     'NOx(GT)' 'PT08.S3(NOx)' 'NO2(GT)' 'PT08.S4(NO2)' 'PT08.S5(O3)' 'T' 'RH' 'AH' \
-    --pretrained --freeze --cross_val 5
+    --pretrained --freeze --cross_val 5 --augmentation
 
 
 python3 main.py 2D --dataset_path datasets/2D --output_var 'CO(GT)' --transform 'ricker'\
     --mode '2D_ViT_SR_feat_in' --gpu 0 --bs 8\
     --variables-to-use 'CO(GT)' 'PT08.S1(CO)' 'C6H6(GT)' 'PT08.S2(NMHC)' \
     'NOx(GT)' 'PT08.S3(NOx)' 'NO2(GT)' 'PT08.S4(NO2)' 'PT08.S5(O3)' 'T' 'RH' 'AH' \
-    --pretrained --freeze --cross_val 5
+    --pretrained --freeze --cross_val 5 --augmentation
 
 
 
