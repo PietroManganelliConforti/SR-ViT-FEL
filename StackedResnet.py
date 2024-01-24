b3fe8ce72eb20e7b93d6ff907db5f0f8990145b3
Pietro Nardelli <pietro.nardelli@outlook.com>
2024-01-24 17:32:13 +0000

    AdaptaBlock implementation

--- StackedResnet.py
+++ StackedResnet.py
@@ -146,9 +146,102 @@ ViTForImageClassification(
 )
 """
 
-# 1. 512 features from stackedresnet into encoder
-# 2. feature maps from stackedresent as images for ViT
-# 3. transforms as images for ViT
+
+
+class AdaptaBlock(nn.Module):
+    def __init__(self, UPCONV=False):
+        super(Block, self).__init__()
+
+        
+        # 512 -> 3
+        self.conv00 = torch.nn.Conv2d(512, 256, kernel_size=1)
+        self.bn00 = nn.BatchNorm2d(256)
+        self.conv01 = torch.nn.Conv2d(256, 128, kernel_size=1)
+        self.bn01 = nn.BatchNorm2d(128)
+        self.conv02 = torch.nn.Conv2d(128, 64, kernel_size=1)
+        self.bn02 = nn.BatchNorm2d(64)
+        self.conv03 = torch.nn.Conv2d(64, 32, kernel_size=1)
+        self.bn03 = nn.BatchNorm2d(32)
+        self.conv04 = torch.nn.Conv2d(32, 1, kernel_size=1)
+        self.bn04 = nn.BatchNorm2d(1) # 12, 16
+
+        # Define ConvTranspose2d and BatchNorm2d layers
+        self.conv_transpose00 = nn.ConvTranspose2d(in_channels=256, out_channels=256, kernel_size=3, stride=2, padding=1) # 23, 31
+        self.bnt00 = nn.BatchNorm2d(256)
+        self.conv_transpose01 = nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1) # 45, 61
+        self.bnt01 = nn.BatchNorm2d(128)
+        self.conv_transpose02 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1) # 89, 121
+        self.bnt02 = nn.BatchNorm2d(64)
+        self.conv_transpose03 = nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1) # 177, 241
+        self.bnt03 = nn.BatchNorm2d(32)
+
+
+        # 256 -> 3
+        self.conv10 = torch.nn.Conv2d(256, 128, kernel_size=1)
+        self.bn10 = nn.BatchNorm2d(128)
+        self.conv11 = torch.nn.Conv2d(128, 64, kernel_size=1)
+        self.bn11 = nn.BatchNorm2d(64)
+        self.conv12 = torch.nn.Conv2d(64, 32, kernel_size=1)
+        self.bn12 = nn.BatchNorm2d(32)
+        self.conv13 = torch.nn.Conv2d(32, 1, kernel_size=1)
+        self.bn13 = nn.BatchNorm2d(1) # 24, 31
+
+        self.conv_transpose10 = nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1) # 47, 61
+        self.bnt10 = nn.BatchNorm2d(128)
+        self.conv_transpose11 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1) # 93, 121
+        self.bnt11 = nn.BatchNorm2d(64)
+        self.conv_transpose12 = nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1) # 185, 241
+        self.bnt12 = nn.BatchNorm2d(32)
+
+        # 128 -> 3
+        self.conv20 = torch.nn.Conv2d(128, 64, kernel_size=1)
+        self.bn20 = nn.BatchNorm2d(64)
+        self.conv21 = torch.nn.Conv2d(64, 32, kernel_size=1)
+        self.bn21 = nn.BatchNorm2d(32)
+        self.conv22 = torch.nn.Conv2d(32, 1, kernel_size=1)
+        self.bn22 = nn.BatchNorm2d(1) # 47, 62 
+
+        self.conv_transpose20 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1) # 93, 123
+        self.bnt20 = nn.BatchNorm2d(64)
+        self.conv_transpose21 = nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1) # 185, 245
+        self.bnt21 = nn.BatchNorm2d(32)
+        
+        self.relu = nn.ReLU()
+
+    def forward (self, tensor0, tensor1, tensor2):
+        # 8, 512 , 12, 16   
+        x = self.relu(self.bn00(self.conv00(tensor0)))
+        x = self.bnt00(self.conv_transpose00(x))
+        x = self.relu(self.bn01(self.conv01(x)))
+        x = self.bnt01(self.conv_transpose01(x))
+        x = self.relu(self.bn02(self.conv02(x)))
+        x = self.bnt02(self.conv_transpose02(x))
+        x = self.relu(self.bn03(self.conv03(x)))
+        x = self.bnt03(self.conv_transpose03(x))
+        x_feat_0 = self.relu(self.bn04(F.interpolate(self.conv04(x), size=(224, 224), mode='bilinear', align_corners=False))) # 8, 1, 224, 224   
+        
+        # 8, 256 , 24, 31
+        x = self.relu(self.bn10(self.conv10(tensor1)))
+        x = self.bnt10(self.conv_transpose10(x))
+        x = self.relu(self.bn11(self.conv11(x)))
+        x = self.bnt11(self.conv_transpose11(x))
+        x = self.relu(self.bn12(self.conv12(x)))
+        x = self.bnt12(self.conv_transpose12(x))
+        x_feat_1 = self.relu(self.bn13(F.interpolate(self.conv13(x), size=(224, 224), mode='bilinear', align_corners=False))) # 8, 1, 224, 224
+
+        # 8 , 128 , 47, 62 
+        x = self.relu(self.bn20(self.conv20(tensor2)))
+        x = self.bnt20(self.conv_transpose20(x))
+        x = self.relu(self.bn21(self.conv21(x)))
+        x = self.bnt21(self.conv_transpose21(x))
+        x_feat_2 = self.relu(self.bn22(F.interpolate(self.conv22(x), size=(224, 224), mode='bilinear', align_corners=False))) # 8, 1, 224, 224
+
+        
+        return torch.cat((x_feat_0,x_feat_1, x_feat_2), axis=1)
+
+
+
+
 class ViTForecaster (nn.Module):
     def __init__(
         self,
@@ -177,23 +270,12 @@ class ViTForecaster (nn.Module):
                                        torch.nn.Dropout(p=0.1),
                                        torch.nn.Linear(1000,outputs,bias=True))        
         
-        # FLAGS
-        self.UPCONV = True
-        
-        
+        self.relu = nn.ReLU()
         if (self.dim == '2D_ViT_im'):
             self.conv1x1 = torch.nn.Conv2d(12, 3, kernel_size=1)
             self.bn1 = nn.BatchNorm2d(3)
         
-        elif (self.dim == '2D_ViT_feat_puzzle'):
-            self.conv1x1_1 = torch.nn.Conv2d(256, 128, kernel_size=1)
-            self.bn1 = nn.BatchNorm2d(128)
-            self.conv1x1_2 = torch.nn.Conv2d(128, 64, kernel_size=1)
-            self.bn2 = nn.BatchNorm2d(64)
-            self.conv1x1_3 = torch.nn.Conv2d(64, 3, kernel_size=1)
-            self.bn3 = nn.BatchNorm2d(3)
-            self.stacked_resnet = stacked_resnet
-            self.stacked_resnet.resnet.fc = nn.Identity()
+
             
         elif (self.dim == '2D_ViT_parallel_SR'):
             self.stacked_resnet = stacked_resnet
@@ -205,81 +287,11 @@ class ViTForecaster (nn.Module):
             
             self.classifier = torch.nn.Sequential(torch.nn.Linear(512*2,24,bias=True))
     
-        elif (self.dim == '2D_ViT_SR_feat_in_puzzle'):
-            self.stacked_resnet = stacked_resnet
-            self.stacked_resnet.resnet.fc = nn.Identity()
-              
-            self.conv1x1_1 = torch.nn.Conv2d(256, 128, kernel_size=1)
-            self.bn1 = nn.BatchNorm2d(128)
-            self.conv1x1_2 = torch.nn.Conv2d(128, 64, kernel_size=1)
-            self.bn2 = nn.BatchNorm2d(64)
-            self.conv1x1_3 = torch.nn.Conv2d(64, 3, kernel_size=1)
-            self.bn3 = nn.BatchNorm2d(3)
-            self.ViT.classifier = torch.nn.Sequential(torch.nn.Linear(768,512,bias=True),torch.nn.Dropout(p=0.1), torch.nn.ReLU())
-              
-            self.classifier = torch.nn.Sequential(torch.nn.Linear(512*2,24,bias=True))
         
         elif (self.dim == '2D_ViT_SR_feat_in'):
             self.stacked_resnet = stacked_resnet
             self.stacked_resnet.resnet.fc = nn.Identity()
-              
-            # 512 -> 3
-            self.conv00 = torch.nn.Conv2d(512, 256, kernel_size=1)
-            self.bn00 = nn.BatchNorm2d(256)
-            self.conv01 = torch.nn.Conv2d(256, 128, kernel_size=1)
-            self.bn01 = nn.BatchNorm2d(128)
-            self.conv02 = torch.nn.Conv2d(128, 64, kernel_size=1)
-            self.bn02 = nn.BatchNorm2d(64)
-            self.conv03 = torch.nn.Conv2d(64, 32, kernel_size=1)
-            self.bn03 = nn.BatchNorm2d(32)
-            self.conv04 = torch.nn.Conv2d(32, 1, kernel_size=1)
-            self.bn04 = nn.BatchNorm2d(1) # 12, 16
-            
-            if (self.UPCONV):
-                # Define ConvTranspose2d and BatchNorm2d layers
-                self.conv_transpose00 = nn.ConvTranspose2d(in_channels=1, out_channels=1, kernel_size=3, stride=2, padding=1) # 23, 31
-                self.bnt00 = nn.BatchNorm2d(1)
-                self.conv_transpose01 = nn.ConvTranspose2d(in_channels=1, out_channels=1, kernel_size=3, stride=2, padding=1) # 45, 61
-                self.bnt01 = nn.BatchNorm2d(1)
-                self.conv_transpose02 = nn.ConvTranspose2d(in_channels=1, out_channels=1, kernel_size=3, stride=2, padding=1) # 89, 121
-                self.bnt02 = nn.BatchNorm2d(1)
-                self.conv_transpose03 = nn.ConvTranspose2d(in_channels=1, out_channels=1, kernel_size=3, stride=2, padding=1) # 177, 241
-                self.bnt03 = nn.BatchNorm2d(1)
-
-            
-            # 256 -> 3
-            self.conv10 = torch.nn.Conv2d(256, 128, kernel_size=1)
-            self.bn10 = nn.BatchNorm2d(128)
-            self.conv11 = torch.nn.Conv2d(128, 64, kernel_size=1)
-            self.bn11 = nn.BatchNorm2d(64)
-            self.conv12 = torch.nn.Conv2d(64, 32, kernel_size=1)
-            self.bn12 = nn.BatchNorm2d(32)
-            self.conv13 = torch.nn.Conv2d(32, 1, kernel_size=1)
-            self.bn13 = nn.BatchNorm2d(1) # 24, 31
-            
-            if (self.UPCONV):
-                self.conv_transpose10 = nn.ConvTranspose2d(in_channels=1, out_channels=1, kernel_size=3, stride=2, padding=1) # 47, 61
-                self.bnt10 = nn.BatchNorm2d(1)
-                self.conv_transpose11 = nn.ConvTranspose2d(in_channels=1, out_channels=1, kernel_size=3, stride=2, padding=1) # 93, 121
-                self.bnt11 = nn.BatchNorm2d(1)
-                self.conv_transpose12 = nn.ConvTranspose2d(in_channels=1, out_channels=1, kernel_size=3, stride=2, padding=1) # 185, 241
-                self.bnt12 = nn.BatchNorm2d(1)
-            
-            # 128 -> 3
-            self.conv20 = torch.nn.Conv2d(128, 64, kernel_size=1)
-            self.bn20 = nn.BatchNorm2d(64)
-            self.conv21 = torch.nn.Conv2d(64, 32, kernel_size=1)
-            self.bn21 = nn.BatchNorm2d(32)
-            self.conv22 = torch.nn.Conv2d(32, 1, kernel_size=1)
-            self.bn22 = nn.BatchNorm2d(1) # 47, 62 
-            
-            # NOT WORKING TO BE FIXED
-            if (self.UPCONV):
-                self.conv_transpose20 = nn.ConvTranspose2d(in_channels=1, out_channels=1, kernel_size=3, stride=2, padding=1) # 93, 123
-                self.bnt20 = nn.BatchNorm2d(1)
-                self.conv_transpose21 = nn.ConvTranspose2d(in_channels=1, out_channels=1, kernel_size=3, stride=2, padding=1) # 185, 245
-                self.bnt21 = nn.BatchNorm2d(1)
-            
+            self.adapta_block = AdaptaBlock()
             
             self.ViT.classifier = torch.nn.Sequential(torch.nn.Linear(768,512,bias=True),torch.nn.Dropout(p=0.1), torch.nn.ReLU())
               
@@ -297,42 +309,6 @@ class ViTForecaster (nn.Module):
             x = F.interpolate(x, size=(224, 224), mode='bilinear', align_corners=False)
             outputs = self.ViT(x, interpolate_pos_encoding=True).logits
            
-        elif (self.dim == '2D_ViT_feat_puzzle'):
-            # Solution 29_11_ViT_feat                
-            
-            tensor1 = get_interested_feature_map(self.stacked_resnet, x_img, list(self.stacked_resnet.resnet.children())[-4][-1].conv1)
-            tensor2 = get_interested_feature_map(self.stacked_resnet, x_img, list(self.stacked_resnet.resnet.children())[-4][-1].conv2)
-            tensor3 = get_interested_feature_map(self.stacked_resnet, x_img, list(self.stacked_resnet.resnet.children())[-4][-2].conv1)
-            tensor4 = get_interested_feature_map(self.stacked_resnet, x_img, list(self.stacked_resnet.resnet.children())[-4][-2].conv2)
-            
-            
-            # Define the new shape (assuming height and width are both doubled)
-            new_height = tensor1.shape[2] * 2
-            new_width = tensor1.shape[3] * 2
-            
-            merged_tensor = torch.zeros(x_img.shape[0], tensor1.shape[1], new_height, new_width).to('cuda')
-            # Fill in each corner with the corresponding tensor
-            merged_tensor[:, :, :tensor1.shape[2], :tensor1.shape[3]] = tensor1  # Upper-left corner
-            merged_tensor[:, :, :tensor2.shape[2], -tensor2.shape[3]:] = tensor2  # Upper-right corner
-            merged_tensor[:, :, -tensor3.shape[2]:, :tensor3.shape[3]] = tensor3  # Lower-left corner
-            merged_tensor[:, :, -tensor4.shape[2]:, -tensor4.shape[3]:] = tensor4  # Lower-right corner
-
-            #print("Merged Tensor Shape:", merged_tensor.shape) # [8, 256, 48, 62]
-         
-            #merged_tensor = torch.cat((tensor1, tensor2, tensor3, tensor4), dim=1)
-            
-            x = self.conv1x1_1(merged_tensor)
-            x = self.bn1(x)
-            x = self.conv1x1_2(x)
-            x = self.bn2(x)
-            x = self.conv1x1_3(x)
-            x = self.bn3(x)
-            
-            
-            # From (8, 12, 396, 496) to (8, 3, 224, 224) 
-            x = F.interpolate(x, size=(224, 224), mode='bilinear', align_corners=False)
-            outputs = self.ViT(x, interpolate_pos_encoding=True).logits
-
         elif (self.dim == '2D_ViT_parallel_SR'):
             x_sr = self.stacked_resnet(x_img)
             
@@ -345,39 +321,6 @@ class ViTForecaster (nn.Module):
             x = torch.cat((x_sr, x_vit), 1)
             outputs = self.classifier(x)
 
-        elif (self.dim == '2D_ViT_SR_feat_in_puzzle'):
-            x_sr = self.stacked_resnet(x_img)
-              
-            #x_feat = get_interested_feature_map(self.stacked_resnet, x_img, list(self.stacked_resnet.resnet.children())[-4][-1].conv2)
-            tensor1 = get_interested_feature_map(self.stacked_resnet, x_img, list(self.stacked_resnet.resnet.children())[-4][-1].conv1) 
-            tensor2 = get_interested_feature_map(self.stacked_resnet, x_img, list(self.stacked_resnet.resnet.children())[-4][-1].conv2) 
-            tensor3 = get_interested_feature_map(self.stacked_resnet, x_img, list(self.stacked_resnet.resnet.children())[-4][-2].conv1)
-            tensor4 = get_interested_feature_map(self.stacked_resnet, x_img, list(self.stacked_resnet.resnet.children())[-4][-2].conv2)
-            
-            
-            # Define the new shape (assuming height and width are both doubled)
-            new_height = tensor1.shape[2] * 2
-            new_width = tensor1.shape[3] * 2
-            
-            merged_tensor = torch.zeros(x_img.shape[0], tensor1.shape[1], new_height, new_width).to('cuda')
-            # Fill in each corner with the corresponding tensor
-            merged_tensor[:, :, :tensor1.shape[2], :tensor1.shape[3]] = tensor1  # Upper-left corner
-            merged_tensor[:, :, :tensor2.shape[2], -tensor2.shape[3]:] = tensor2  # Upper-right corner
-            merged_tensor[:, :, -tensor3.shape[2]:, :tensor3.shape[3]] = tensor3  # Lower-left corner
-            merged_tensor[:, :, -tensor4.shape[2]:, -tensor4.shape[3]:] = tensor4  # Lower-right corner
-
-            
-            x_feat = F.interpolate(merged_tensor, size=(224, 224), mode='bilinear', align_corners=False)
-            x_feat = self.conv1x1_1(x_feat)
-            x_feat = self.bn1(x_feat)
-            x_feat = self.conv1x1_2(x_feat)
-            x_feat = self.bn2(x_feat)
-            x_feat = self.conv1x1_3(x_feat)
-            x_feat = self.bn3(x_feat)
-            x_vit = self.ViT(x_feat).logits
-              
-            x = torch.cat((x_sr, x_vit), 1)
-            outputs = self.classifier(x)
             
         elif (self.dim == '2D_ViT_SR_feat_in'):
             x_sr = self.stacked_resnet(x_img)
@@ -387,64 +330,11 @@ class ViTForecaster (nn.Module):
             tensor1 = get_interested_feature_map(self.stacked_resnet, x_img, list(self.stacked_resnet.resnet.children())[-4][-1].conv2) # 8 256 24 31
             tensor2 = get_interested_feature_map(self.stacked_resnet, x_img, list(self.stacked_resnet.resnet.children())[-5][-1].conv2) # 8 128 47 62
 
-            
-            x_feat_0 = self.conv00(tensor0)
-            x_feat_0 = self.bn00(x_feat_0)
-            x_feat_0 = self.conv01(x_feat_0)
-            x_feat_0 = self.bn01(x_feat_0)
-            x_feat_0 = self.conv02(x_feat_0)
-            x_feat_0 = self.bn02(x_feat_0)
-            x_feat_0 = self.conv03(x_feat_0)
-            x_feat_0 = self.bn03(x_feat_0)
-            x_feat_0 = self.conv04(x_feat_0)
-            x_feat_0 = self.bn04(x_feat_0) # 8, 1 , 12, 16
-            if (self.UPCONV):
-                x_feat_0 = self.conv_transpose00(x_feat_0)
-                x_feat_0 = self.bnt00(x_feat_0)
-                x_feat_0 = self.conv_transpose01(x_feat_0)
-                x_feat_0 = self.bnt01(x_feat_0)
-                x_feat_0 = self.conv_transpose02(x_feat_0)
-                x_feat_0 = self.bnt02(x_feat_0)
-                x_feat_0 = self.conv_transpose03(x_feat_0)   
-                x_feat_0 = self.bnt03(x_feat_0)
-            x_feat_0 = F.interpolate(x_feat_0, size=(224, 224), mode='bilinear', align_corners=False) # 8, 1, 224, 224
-                
-            x_feat_1 = self.conv10(tensor1)
-            x_feat_1 = self.bn10(x_feat_1)
-            x_feat_1 = self.conv11(x_feat_1)
-            x_feat_1 = self.bn11(x_feat_1)
-            x_feat_1 = self.conv12(x_feat_1)
-            x_feat_1 = self.bn12(x_feat_1)
-            x_feat_1 = self.conv13(x_feat_1)
-            x_feat_1 = self.bn13(x_feat_1) # 8, 1 , 24, 31
-            if (self.UPCONV):
-                x_feat_1 = self.conv_transpose10(x_feat_1)
-                x_feat_1 = self.bnt10(x_feat_1)
-                x_feat_1 = self.conv_transpose11(x_feat_1)
-                x_feat_1 = self.bnt11(x_feat_1)
-                x_feat_1 = self.conv_transpose12(x_feat_1)
-                x_feat_1 = self.bnt12(x_feat_1)
-            x_feat_1 = F.interpolate(x_feat_1, size=(224, 224), mode='bilinear', align_corners=False) # 8, 1, 224, 224
-            
-            x_feat_2 = self.conv20(tensor2)
-            x_feat_2 = self.bn20(x_feat_2)
-            x_feat_2 = self.conv21(x_feat_2)
-            x_feat_2 = self.bn21(x_feat_2)
-            x_feat_2 = self.conv22(x_feat_2)
-            x_feat_2 = self.bn22(x_feat_2) # 8 , 1 , 47, 62 
-            if (self.UPCONV):
-                x_feat_2 = self.conv_transpose20(x_feat_2)
-                x_feat_2 = self.bnt20(x_feat_2)
-                x_feat_2 = self.conv_transpose21(x_feat_2)
-                x_feat_2 = self.bnt21(x_feat_2)
-            x_feat_2 = F.interpolate(x_feat_2, size=(224, 224), mode='bilinear', align_corners=False) # 8, 1, 224, 224
-            
-            x_feat_merged = torch.cat((x_feat_0,x_feat_1, x_feat_2), axis=1)
+            x_feat_merged = self.adapta_block(tensor0, tensor1, tensor2)
             x_vit = self.ViT(x_feat_merged).logits
               
             x = torch.cat((x_sr, x_vit), 1)
-            outputs = self.classifier(x)
-            
+            outputs = self.classifier(x)            
         
         return outputs
 
