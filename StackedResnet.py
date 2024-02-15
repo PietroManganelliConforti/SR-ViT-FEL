3bb9bbf55d8439fdcf8bd4c34c0beaf9f10af573
Pietro Nardelli <pietro.nardelli@outlook.com>
2024-02-15 08:44:40 +0000

    Up-down inverted and dropout increased

--- StackedResnet.py
+++ StackedResnet.py
@@ -166,14 +166,14 @@ class AdaptaBlock(nn.Module):
         self.bn04 = nn.BatchNorm2d(1) # 12, 16
 
         # Define ConvTranspose2d and BatchNorm2d layers
-        self.conv_transpose00 = nn.ConvTranspose2d(in_channels=256, out_channels=256, kernel_size=3, stride=2, padding=1) # 23, 31
-        self.bnt00 = nn.BatchNorm2d(256)
-        self.conv_transpose01 = nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1) # 45, 61
-        self.bnt01 = nn.BatchNorm2d(128)
-        self.conv_transpose02 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1) # 89, 121
-        self.bnt02 = nn.BatchNorm2d(64)
-        self.conv_transpose03 = nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1) # 177, 241
-        self.bnt03 = nn.BatchNorm2d(32)
+        self.conv_transpose00 = nn.ConvTranspose2d(in_channels=512, out_channels=512, kernel_size=3, stride=2, padding=1) # 23, 31
+        self.bnt00 = nn.BatchNorm2d(512)
+        self.conv_transpose01 = nn.ConvTranspose2d(in_channels=256, out_channels=256, kernel_size=3, stride=2, padding=1) # 45, 61
+        self.bnt01 = nn.BatchNorm2d(256)
+        self.conv_transpose02 = nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1) # 89, 121
+        self.bnt02 = nn.BatchNorm2d(128)
+        self.conv_transpose03 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1) # 177, 241
+        self.bnt03 = nn.BatchNorm2d(64)
 
 
         # 256 -> 3
@@ -186,12 +186,12 @@ class AdaptaBlock(nn.Module):
         self.conv13 = torch.nn.Conv2d(32, 1, kernel_size=1)
         self.bn13 = nn.BatchNorm2d(1) # 24, 31
 
-        self.conv_transpose10 = nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1) # 47, 61
-        self.bnt10 = nn.BatchNorm2d(128)
-        self.conv_transpose11 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1) # 93, 121
-        self.bnt11 = nn.BatchNorm2d(64)
-        self.conv_transpose12 = nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1) # 185, 241
-        self.bnt12 = nn.BatchNorm2d(32)
+        self.conv_transpose10 = nn.ConvTranspose2d(in_channels=256, out_channels=256, kernel_size=3, stride=2, padding=1) # 47, 61
+        self.bnt10 = nn.BatchNorm2d(256)
+        self.conv_transpose11 = nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1) # 93, 121
+        self.bnt11 = nn.BatchNorm2d(128)
+        self.conv_transpose12 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1) # 185, 241
+        self.bnt12 = nn.BatchNorm2d(64)
 
         # 128 -> 3
         self.conv20 = torch.nn.Conv2d(128, 64, kernel_size=1)
@@ -201,14 +201,16 @@ class AdaptaBlock(nn.Module):
         self.conv22 = torch.nn.Conv2d(32, 1, kernel_size=1)
         self.bn22 = nn.BatchNorm2d(1) # 47, 62 
 
-        self.conv_transpose20 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1) # 93, 123
-        self.bnt20 = nn.BatchNorm2d(64)
-        self.conv_transpose21 = nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1) # 185, 245
-        self.bnt21 = nn.BatchNorm2d(32)
+        self.conv_transpose20 = nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1) # 93, 123
+        self.bnt20 = nn.BatchNorm2d(128)
+        self.conv_transpose21 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1) # 185, 245
+        self.bnt21 = nn.BatchNorm2d(64)
         
         self.relu = nn.ReLU()
 
     def forward (self, tensor0, tensor1, tensor2):
+        # ORIGINAL: downlsampling and upsampling
+        """
         # 8, 512 , 12, 16   
         x = self.relu(self.bn00(self.conv00(tensor0)))
         x = self.bnt00(self.conv_transpose00(x))
@@ -235,7 +237,35 @@ class AdaptaBlock(nn.Module):
         x = self.relu(self.bn21(self.conv21(x)))
         x = self.bnt21(self.conv_transpose21(x))
         x_feat_2 = self.relu(self.bn22(F.interpolate(self.conv22(x), size=(224, 224), mode='bilinear', align_corners=False))) # 8, 1, 224, 224
+        """
+        # MODIFIED: upsampling and downsampling
+        
+        # 8, 512 , 12, 16   
+        x = self.bnt00(self.conv_transpose00(tensor0))
+        x = self.relu(self.bn00(self.conv00(x)))
+        x = self.bnt01(self.conv_transpose01(x))
+        x = self.relu(self.bn01(self.conv01(x)))
+        x = self.bnt02(self.conv_transpose02(x))
+        x = self.relu(self.bn02(self.conv02(x)))
+        x = self.bnt03(self.conv_transpose03(x))
+        x = self.relu(self.bn03(self.conv03(x)))
+        x_feat_0 = self.relu(self.bn04(F.interpolate(self.conv04(x), size=(224, 224), mode='bilinear', align_corners=False))) # 8, 1, 224, 224   
+        
+        # 8, 256 , 24, 31
+        x = self.bnt10(self.conv_transpose10(tensor1))
+        x = self.relu(self.bn10(self.conv10(x)))
+        x = self.bnt11(self.conv_transpose11(x))
+        x = self.relu(self.bn11(self.conv11(x)))
+        x = self.bnt12(self.conv_transpose12(x))
+        x = self.relu(self.bn12(self.conv12(x)))
+        x_feat_1 = self.relu(self.bn13(F.interpolate(self.conv13(x), size=(224, 224), mode='bilinear', align_corners=False))) # 8, 1, 224, 224
 
+        # 8 , 128 , 47, 62 
+        x = self.bnt20(self.conv_transpose20(tensor2))
+        x = self.relu(self.bn20(self.conv20(x)))
+        x = self.bnt21(self.conv_transpose21(x))
+        x = self.relu(self.bn21(self.conv21(x)))
+        x_feat_2 = self.relu(self.bn22(F.interpolate(self.conv22(x), size=(224, 224), mode='bilinear', align_corners=False))) # 8, 1, 224, 224
         
         return torch.cat((x_feat_0,x_feat_1, x_feat_2), axis=1)
 
@@ -266,10 +296,10 @@ class ViTForecaster (nn.Module):
         
         self.ViT = transformers.ViTForImageClassification.from_pretrained(model_name, config=config, ignore_mismatched_sizes=True)        
         #model.vit.embeddings.patch_embeddings.projection = torch.nn.Conv2d(512, 768, kernel_size=(8, 1), stride=(8, 1), padding=(0,0), groups=256)
-        self.ViT.classifier=torch.nn.Sequential(torch.nn.Linear(768,1000,bias=True),
-                                       torch.nn.Dropout(p=0.1),
-                                       torch.nn.Linear(1000,outputs,bias=True))        
-        
+        self.ViT.classifier = torch.nn.Sequential(torch.nn.Linear(768,512,bias=True),torch.nn.Dropout(p=0.5), torch.nn.ReLU())
+        self.classifier = torch.nn.Sequential(torch.nn.Linear(512*2,24,bias=True))
+
+    
         self.relu = nn.ReLU()
         if (self.dim == '2D_ViT_im'):
             self.conv1x1 = torch.nn.Conv2d(12, 3, kernel_size=1)
@@ -282,10 +312,7 @@ class ViTForecaster (nn.Module):
             self.stacked_resnet.resnet.fc = nn.Identity()
             
             self.conv1x1 = torch.nn.Conv2d(12, 3, kernel_size=1)
-            self.bn1 = nn.BatchNorm2d(3)
-            self.ViT.classifier = torch.nn.Sequential(torch.nn.Linear(768,512,bias=True),torch.nn.Dropout(p=0.1), torch.nn.ReLU())
-            
-            self.classifier = torch.nn.Sequential(torch.nn.Linear(512*2,24,bias=True))
+            self.bn1 = nn.BatchNorm2d(3)            
     
         
         elif (self.dim == '2D_ViT_SR_feat_in'):
@@ -293,9 +320,7 @@ class ViTForecaster (nn.Module):
             self.stacked_resnet.resnet.fc = nn.Identity()
             self.adapta_block = AdaptaBlock()
             
-            self.ViT.classifier = torch.nn.Sequential(torch.nn.Linear(768,512,bias=True),torch.nn.Dropout(p=0.1), torch.nn.ReLU())
               
-            self.classifier = torch.nn.Sequential(torch.nn.Linear(512*2,24,bias=True))
             
 
     # x_img: (N, H, W, C)
