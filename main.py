5d1ab0804caee9ce5c91fc68d70a9aca190e0f67
Pietro Nardelli <pietro.nardelli@outlook.com>
2024-03-20 18:08:44 +0000

    additional seeds and num_workers=0

--- main.py
+++ main.py
@@ -66,9 +66,9 @@ def collect_data_2D(data_path , transform, device, output_var, train_test_split,
         fold_train_dataset = torch.utils.data.Subset(dataset, train_indices)
         fold_val_dataset = torch.utils.data.Subset(dataset, test_indices)
         
-        train_data_loader = torch.utils.data.DataLoader(fold_train_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
-        val_data_loader = torch.utils.data.DataLoader(fold_val_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
-        test_data_loader = torch.utils.data.DataLoader(fold_val_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
+        train_data_loader = torch.utils.data.DataLoader(fold_train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+        val_data_loader = torch.utils.data.DataLoader(fold_val_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+        test_data_loader = torch.utils.data.DataLoader(fold_val_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
 
         print(f'There are: {len(fold_train_dataset)} training samples, {len(fold_val_dataset)} validation samples and {len(fold_val_dataset)} test samples')
         print(f'start: {start}, end: {end}, len_dataset: {len_dataset}')
@@ -83,9 +83,9 @@ def collect_data_2D(data_path , transform, device, output_var, train_test_split,
         print(f'Ther are: {len(train_dataset)} training samples, {len(val_dataset)} validation samples and {len(test_dataset)} test samples')
 
         # create dataloaders
-        train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
-        val_data_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
-        test_data_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
+        train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+        val_data_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+        test_data_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
 
     return train_data_loader, val_data_loader, test_data_loader
 
@@ -148,9 +148,9 @@ def collect_data_2D_lstm(data_path , transform, device, output_var, train_test_s
         fold_train_dataset = torch.utils.data.Subset(dataset_zipped, train_indices)
         fold_val_dataset = torch.utils.data.Subset(dataset_zipped, test_indices)
         
-        train_data_loader = torch.utils.data.DataLoader(fold_train_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
-        val_data_loader = torch.utils.data.DataLoader(fold_val_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
-        test_data_loader = torch.utils.data.DataLoader(fold_val_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
+        train_data_loader = torch.utils.data.DataLoader(fold_train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+        val_data_loader = torch.utils.data.DataLoader(fold_val_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+        test_data_loader = torch.utils.data.DataLoader(fold_val_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
 
         print(f'There are: {len(fold_train_dataset)} training samples, {len(fold_val_dataset)} validation samples and {len(fold_val_dataset)} test samples')
         print(f'start: {start}, end: {end}, len_dataset: {len_dataset}')
@@ -158,9 +158,9 @@ def collect_data_2D_lstm(data_path , transform, device, output_var, train_test_s
     
     else:
         
-        train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
-        val_data_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
-        test_data_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
+        train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+        val_data_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+        test_data_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
 
     return train_data_loader, val_data_loader, test_data_loader, output_var_idx
 
@@ -181,9 +181,9 @@ def collect_data_1D(data_path, csv_file, device, train_test_split, train_val_spl
     print(f'There are: {len(train_dataset)} training samples, {len(val_dataset)} validation samples and {len(test_dataset)} test samples')
 
     # create dataloaders
-    train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
-    val_data_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
-    test_data_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
+    train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+    val_data_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+    test_data_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
 
     return train_data_loader, val_data_loader, test_data_loader
 
@@ -426,12 +426,24 @@ def main_1d(args, cross_validation_idx=-1):
     # Seed #
 
     seed = 0
-
+    
     os.environ["PYTHONHASHSEED"] = str(seed)
-
+    
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+    
+    random.seed(seed)
+    
+    np.random.seed(seed)
+    
     torch.manual_seed(seed)
-
+    
     torch.cuda.manual_seed(seed)
+    
+    torch.backends.cudnn.enabled=False
+    
+    torch.backends.cudnn.deterministic=True
+    
+    torch.set_deterministic(True)
 
     ####### ARGS
     
@@ -556,12 +568,24 @@ def main_2d(args, cross_validation_idx=-1):
     # Seed #
 
     seed = 0
-
+    
     os.environ["PYTHONHASHSEED"] = str(seed)
-
+    
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+    
+    random.seed(seed)
+    
+    np.random.seed(seed)
+    
     torch.manual_seed(seed)
-
+    
     torch.cuda.manual_seed(seed)
+    
+    torch.backends.cudnn.enabled=False
+    
+    torch.backends.cudnn.deterministic=True
+    
+    torch.set_deterministic(True)
 
     ####### ARGS
 
@@ -651,13 +675,25 @@ def main_2d_lstm(args, cross_validation_idx=-1):
     # Seed #
 
     seed = 0
-
+    
     os.environ["PYTHONHASHSEED"] = str(seed)
-
+    
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+    
+    random.seed(seed)
+    
+    np.random.seed(seed)
+    
     torch.manual_seed(seed)
-
+    
     torch.cuda.manual_seed(seed)
-
+    
+    torch.backends.cudnn.enabled=False
+    
+    torch.backends.cudnn.deterministic=True
+    
+    torch.set_deterministic(True)
+    
     ####### ARGS
 
     system_time = time.localtime()
